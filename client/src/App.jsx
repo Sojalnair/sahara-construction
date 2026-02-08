08ded153b61bbafc5f77128c04a85e05344391dd
Sojalnair <145917458+Sojalnair@users.noreply.github.com>
2026-02-08 13:16:06 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './App.css';
 
 // API Base URL
-const API_URL = 'https://sahara-construction.onrender.com/';
+const API_URL = 'https://sahara-construction.onrender.com';
 
 // Axios instance
 const api = axios.create({
